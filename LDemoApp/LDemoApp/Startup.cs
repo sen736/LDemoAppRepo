9dc257eee0444a54a0f47d8e09a68cab5a1525f3
sen736 <SXK55@lennoxintl.com>
2017-05-10 10:52:06 +0530

    Initial Checkin
    Initial project checkin

---
+++ LDemoApp/LDemoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LDemoApp.Startup))]
+namespace LDemoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
